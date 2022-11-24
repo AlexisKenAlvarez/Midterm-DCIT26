3e022ee7f875c60ce3fdb4f5b0d3387b1bef6128
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-11-24 16:35:47 +0800

    Added button

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,12 +8,38 @@ const moretext = document.getElementById("more-text")
 const moreline = document.getElementById("more-line")
 
 const arrow = document.getElementById("arrow")
+const button = document.getElementById("button")
+
+button.addEventListener("click", () => {
+    button.style.transform = "translateY(5px)"
+    button.style.boxShadow = "none"
+
+    function animateButton(callback) {
+        setTimeout(() => {
+            button.style.transform = "translateY(0)"
+            button.style.boxShadow = "1px 5px 0px 0px rgba(50,50,50,1)"
+            callback()
+        }, 100);
+    }
+
+    function showProfile() {
+        setTimeout(() => {
+            slider.style.opacity = "1";
+            slider.style.pointerEvents = "auto"
+        }, 100);
+
+    }
+
+    animateButton(showProfile)
+
+})
+
+
 
 
 more.addEventListener("click", () => {
     show = true
     slider.style.opacity = "1";
-
     slider.style.pointerEvents = "auto"
 
 })
